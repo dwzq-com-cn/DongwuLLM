dfbbdec55900d552131b1a31ede0243503988846
dwzqshowcai <160448421+dwzq-com-cn@users.noreply.github.com>
2024-03-12 00:00:37 +0800

    Delete pruner directory

--- pruner/docker_commands.sh
+++
@@ -1,5 +0,0 @@
-docker run --gpus all  --shm-size=64g --net=host -dit --rm --name megatron -v /data:/data -v /root/.ssh:/root/.ssh nvcr.io/nvidia/pytorch:23.12-py3
-
-docker exec -it megatron bash
-
-docker stop megatron
--- pruner/scripts/transfer.sh
+++
@@ -1,8 +0,0 @@
-
-python tools/checkpoint/util.py \
-        --model-type GPT \
-        --load-dir /data/megatron_ckpt/test_prune_3B \
-        --save-dir /data/megatron_ckpt/test_pruned_3B_tp1 \
-        --target-tensor-parallel-size 1 \
-        --target-pipeline-parallel-size 1 \
-        --megatron-path /data/Megatron-LM-main
